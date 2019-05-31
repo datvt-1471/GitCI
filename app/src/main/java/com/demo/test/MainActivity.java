35d4fabbbb25cbc72e2b4e74164d3c2382692808
datvt-1471 <vu.tien.dat@sun-asterisk.com>
2019-05-31 14:04:22 +0700

    test sdk

--- app/src/main/java/com/demo/test/MainActivity.java
+++ app/src/main/java/com/demo/test/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "abce", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "abcef", Toast.LENGTH_SHORT).show();
     }
 }
